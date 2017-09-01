d42df48b9e389dd3f490a291348b4e70786bcbbc
Andrew Minion <andrew@andrewrminion.com>
2017-09-01 17:04:17 -0400

    add footer credits

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -10,5 +10,5 @@
 
 ?>
 <div class="site-info">
-    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
+    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" target="_blank" rel="noopener"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a> | Designed and developed by <a href="https://andrewrminion.com/?ref=greathomeschoolconventions.com" target="_blank" rel="noopener">AndrewRMinion Design</a>
 </div><!-- .site-info -->

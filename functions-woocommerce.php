fb846568d2d81297f785ee37263e6187e5b4096e
Andrew Minion <andrew@andrewrminion.com>
2017-09-15 15:02:48 -0400

    update homepage locations stripe URLs

--- functions-woocommerce.php
+++ functions-woocommerce.php
@@ -13,7 +13,7 @@ function ghc_locations_stripe( $info_text = 'More Information&rarr;' ) {
         <div class="wrap full">
             <?php foreach ( $conventions as $convention ) { ?>
             <div class="<?php echo strtolower( $convention['convention_abbreviated_name'][0] ); ?> theme bg">
-                <a href="<?php echo get_permalink( $convention['registration'][0] ); ?>" data-post-id="<?php echo $convention['registration'][0]; ?>" data-post-category="<?php echo strtolower( $convention['convention_short_name'][0] ); ?>">
+                <a href="<?php echo get_permalink( $convention['ID'] ); ?>" data-post-id="<?php echo $convention['registration'][0]; ?>" data-post-category="<?php echo strtolower( $convention['convention_short_name'][0] ); ?>">
                     <h2><?php echo $convention['convention_short_name'][0]; ?></h2>
                     <p class="info"><?php echo ghc_format_date_range( $convention['begin_date'][0], $convention['end_date'][0], 'Ymd' ); ?></p>
                     <p><?php echo esc_attr( $info_text ); ?></p>

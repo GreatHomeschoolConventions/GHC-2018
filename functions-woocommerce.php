7fa8fb60b5084ff37451b62c9c98cbde0b76e84c
Andrew Minion <andrew@andrewrminion.com>
2017-09-26 09:40:37 -0400

    add city/state to homepage locations

--- functions-woocommerce.php
+++ functions-woocommerce.php
@@ -15,6 +15,7 @@ function ghc_locations_stripe( $info_text = 'More Information&rarr;' ) {
             <div class="<?php echo strtolower( $convention['convention_abbreviated_name'][0] ); ?> theme bg">
                 <a href="<?php echo get_permalink( $convention['ID'] ); ?>" data-post-id="<?php echo $convention['registration'][0]; ?>" data-post-category="<?php echo strtolower( $convention['convention_short_name'][0] ); ?>">
                     <h2><?php echo $convention['convention_short_name'][0]; ?></h2>
+                    <p class="info"><?php echo $convention['city'][0] . ', ' . $convention['state'][0]; ?></p>
                     <p class="info"><?php echo ghc_format_date_range( $convention['begin_date'][0], $convention['end_date'][0], 'Ymd' ); ?></p>
                     <p><?php echo esc_attr( $info_text ); ?></p>
                 </a>

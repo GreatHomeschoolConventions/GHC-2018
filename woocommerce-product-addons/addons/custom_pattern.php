20ba231efd43f0dae62540d905019ee73622b9ad
Andrew Minion <andrew@andrewrminion.com>
2017-10-03 16:38:02 -0400

    set default value for addons to “2”

--- woocommerce-product-addons/addons/custom_pattern.php
+++ woocommerce-product-addons/addons/custom_pattern.php
@@ -1,7 +1,7 @@
 <?php foreach ( $addon['options'] as $key => $option ) :
     $addon_key     = 'addon-' . sanitize_title( $addon['field-name'] );
     $option_key    = empty( $option['label'] ) ? $key : sanitize_title( $option['label'] );
-    $current_value = isset( $_POST[ $addon_key ] ) && isset( $_POST[ $addon_key ][ $option_key ] ) ? wc_clean( $_POST[ $addon_key ][ $option_key ] ) : '';
+    $current_value = isset( $_POST[ $addon_key ] ) && isset( $_POST[ $addon_key ][ $option_key ] ) ? wc_clean( $_POST[ $addon_key ][ $option_key ] ) : '2';
     $price = apply_filters( 'woocommerce_product_addons_option_price',
         $option['price'] > 0 ? '(' . wc_price( get_product_addon_price_for_display( $option['price'] ) ) . ')' : '',
         $option,

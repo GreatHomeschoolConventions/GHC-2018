f6acce3bb7447029de44449e0eeb727acdd44a36
Andrew Minion <andrew@andrewrminion.com>
2017-10-03 16:37:53 -0400

    add placeholder to product addons

--- woocommerce-product-addons/addons/custom_pattern.php
+++ woocommerce-product-addons/addons/custom_pattern.php
@@ -8,13 +8,21 @@
         $key,
         'custom_pattern'
     );
+
+    // set placeholder
+    $placeholder = '';
+    if ( strpos( $addon_key, 'family-members' ) !== false ) {
+        $placeholder = '1';
+    }
     ?>
 
     <p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
-        <?php if ( ! empty( $option['label'] ) ) : ?>
-            <label><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?></label>
-        <?php endif; ?>
-        <input type="text" pattern="<?php echo esc_attr( $pattern ); ?>" title="<?php echo esc_attr( $title ); ?>" class="input-text addon addon-custom addon-custom-pattern" data-raw-price="<?php echo esc_attr( $option['price'] ); ?>" data-price="<?php echo get_product_addon_price_for_display( $option['price'] ); ?>" name="<?php echo $addon_key ?>[<?php echo $option_key; ?>]" value="<?php echo esc_attr( $current_value ); ?>" <?php if ( ! empty( $option['max'] ) ) echo 'maxlength="' . $option['max'] .'"'; ?> />
+        <label>
+            <input type="text" pattern="<?php echo esc_attr( $pattern ); ?>" title="<?php echo esc_attr( $title ); ?>" class="input-text addon addon-custom addon-custom-pattern" data-raw-price="<?php echo esc_attr( $option['price'] ); ?>" data-price="<?php echo get_product_addon_price_for_display( $option['price'] ); ?>" name="<?php echo $addon_key ?>[<?php echo $option_key; ?>]" value="<?php echo esc_attr( $current_value ); ?>" placeholder="<?php echo $placeholder; ?>" <?php if ( ! empty( $option['max'] ) ) echo 'maxlength="' . $option['max'] .'"'; ?> />
+            <?php if ( ! empty( $option['label'] ) ) : ?>
+                <?php echo wptexturize( $option['label'] ) . ' ' . $price; ?>
+            <?php endif; ?>
+        </label>
     </p>
 
 <?php endforeach; ?>

60e45e81c95ab108d3c8b010a725eb35ac463805
Andrew Minion <andrew@andrewrminion.com>
2017-10-04 09:21:05 -0400

    add cache-busting stylesheet version

--- functions.php
+++ functions.php
@@ -1,12 +1,14 @@
 <?php
 
+CONST GHC_THEME_VERSION = '3.0.1';
+
 /**
  * Add minified stylesheet, webfonts, and other custom assets
  */
 function ghc_2018_minified_css() {
     wp_enqueue_style( 'local-webfonts', 'https://cdn.greathomeschoolconventions.com/fonts/league-gothic-normal.css' );
     wp_enqueue_style( 'google-webfonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i' );
-    wp_enqueue_style( 'twentyseventeen-style', get_stylesheet_directory_uri() . '/style.min.css' );
+    wp_enqueue_style( 'twentyseventeen-style', get_stylesheet_directory_uri() . '/style.min.css', array(), GHC_THEME_VERSION );
     wp_enqueue_style( 'dashicons' );
 }
 add_action( 'wp_enqueue_scripts', 'ghc_2018_minified_css', 5 );

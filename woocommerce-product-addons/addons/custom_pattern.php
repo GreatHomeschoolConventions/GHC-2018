6af3054f8080ba4899ac61356b55e93ce01a79db
Andrew Minion <andrew@andrewrminion.com>
2017-10-03 22:01:42 -0400

    set extra attributes

--- woocommerce-product-addons/addons/custom_pattern.php
+++ woocommerce-product-addons/addons/custom_pattern.php
@@ -9,16 +9,20 @@
         'custom_pattern'
     );
 
-    // set placeholder
+    // set extra attributes
+    $input_type = 'text';
+    $extra_attributes = '';
     $placeholder = '';
     if ( strpos( $addon_key, 'family-members' ) !== false ) {
         $placeholder = '1';
+        $input_type = 'number';
+        $extra_attributes = 'min="1" max=20"';
     }
     ?>
 
     <p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
         <label>
-            <input type="text" pattern="<?php echo esc_attr( $pattern ); ?>" title="<?php echo esc_attr( $title ); ?>" class="input-text addon addon-custom addon-custom-pattern" data-raw-price="<?php echo esc_attr( $option['price'] ); ?>" data-price="<?php echo get_product_addon_price_for_display( $option['price'] ); ?>" name="<?php echo $addon_key ?>[<?php echo $option_key; ?>]" value="<?php echo esc_attr( $current_value ); ?>" placeholder="<?php echo $placeholder; ?>" <?php if ( ! empty( $option['max'] ) ) echo 'maxlength="' . $option['max'] .'"'; ?> />
+            <input type="<?php echo $input_type ?>" pattern="<?php echo esc_attr( $pattern ); ?>" title="<?php echo esc_attr( $title ); ?>" class="input-text addon addon-custom addon-custom-pattern" data-raw-price="<?php echo esc_attr( $option['price'] ); ?>" data-price="<?php echo get_product_addon_price_for_display( $option['price'] ); ?>" name="<?php echo $addon_key ?>[<?php echo $option_key; ?>]" value="<?php echo esc_attr( $current_value ); ?>" placeholder="<?php echo $placeholder; ?>" <?php if ( ! empty( $option['max'] ) ) echo 'maxlength="' . $option['max'] .'"'; ?> <?php echo $extra_attributes; ?> />
             <?php if ( ! empty( $option['label'] ) ) : ?>
                 <?php echo wptexturize( $option['label'] ) . ' ' . $price; ?>
             <?php endif; ?>

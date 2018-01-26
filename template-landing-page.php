64efb0c211b2d7cc6462f5067cc5ce2d7514b1cc
Andrew Minion <andrew@andrewrminion.com>
2018-01-26 13:33:39 -0500

    add landing page template

---
+++ template-landing-page.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: Landing Page
+ *
+ * The template for displaying custom landing pages
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+            <?php
+            while ( have_posts() ) : the_post();
+            ?>
+
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                        <?php twentyseventeen_edit_link( get_the_ID() ); ?>
+                    </header><!-- .entry-header -->
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div><!-- .entry-content -->
+                </article><!-- #post-## -->
+
+            <?php
+            endwhile; // End of the loop.
+            ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();

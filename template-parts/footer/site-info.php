a50852ffc6748c05c3d6024edc19d3a13f96a110
Andrew Minion <andrew@andrewrminion.com>
2018-05-18 09:07:19 -0400

    add privacy policy link to footer

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -10,5 +10,10 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" target="_blank" rel="noopener"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a> | Designed and developed by <a href="https://andrewrminion.com/?ref=greathomeschoolconventions.com" target="_blank" rel="noopener">AndrewRMinion Design</a>
+	<?php
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link( '', ' |' );
+	}
+	?>
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" target="_blank" rel="noopener">Proudly powered by WordPress</a> | Designed and developed by <a href="https://andrewrminion.com/?ref=greathomeschoolconventions.com" target="_blank" rel="noopener">AndrewRMinion Design</a>
 </div><!-- .site-info -->
